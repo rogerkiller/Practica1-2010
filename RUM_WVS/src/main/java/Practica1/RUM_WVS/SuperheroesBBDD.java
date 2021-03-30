5bec55751f9494a93f4e5b6bc57c669ac744d180
rogerkiller <rogelito.uribe@gmail.com>
2021-03-30 17:45:03 +0200

    Cambios Roger

--- RUM_WVS/src/main/java/Practica1/RUM_WVS/SuperheroesBBDD.java
+++ RUM_WVS/src/main/java/Practica1/RUM_WVS/SuperheroesBBDD.java
@@ -55,13 +55,17 @@ public class SuperheroesBBDD {
 
 			this.BBDDid = node1.getChildText("BBDDid");
 
-			Element node2 = rootNode.getChild("Clientes");
-			for (Element node21 : node2.getChildren("Cliente")) {
-				for (Element node211 : node21.getChildren("Direccion")) {
-					for (int i = 0; i < heroes.size(); i++) {
-						String s = node21.getChildText("HeroeID");
-						heroes.add(new Heroes(Integer.parseInt(node21.getChildText("HeroeID")), node21.getChildText("NombreHeroe"), node21.getChildText("SecretID"), node21.getChildText("Genero"), node21.getChildText("Procedencia"), new ArrayList<Batallas>(), new ArrayList<Habilidades>()));
-						heroes.get(i).getBatallas().add(new Batallas(Integer.parseInt(node211.getChildText("IDBatalla")), node21.getChildText("Lugar"), new SimpleDateFormat("dd/MM/yyyy").parse(node21.getChildText("Genero")), new SimpleDateFormat("dd/MM/yyyy").parse(node21.getChildText("Genero"))));
+			Element node2 = rootNode.getChild("Heroes");
+			for (Element node21 : node2.getChildren("Heroe")) {
+				for (Element node211 : node21.getChildren("Batallas")) {
+					for (Element node2111 : node21.getChildren("Habilidades")) {
+						for (int i = 0; i < heroes.size(); i++) {
+							String s = node21.getChildText("HeroeID");
+							heroes.add(new Heroes(Integer.parseInt(node21.getChildText("HeroeID")), node21.getChildText("NombreHeroe"), node21.getChildText("SecretID"), 
+									node21.getChildText("Genero"), node21.getChildText("Procedencia"), new ArrayList<Batallas>(), new ArrayList<Habilidades>()));
+							heroes.get(i).getBatallas().add(new Batallas(Integer.parseInt(node211.getChildText("IDBatalla")), node21.getChildText("Lugar"), 
+									new SimpleDateFormat("dd/MM/yyyy").parse(node21.getChildText("Genero")), new SimpleDateFormat("dd/MM/yyyy").parse(node21.getChildText("Genero"))));
+						}
 					}	
 				}
 			}
